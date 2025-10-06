57f8f1d4563747cd7d216f6a9bdee08f795ad614
Minh <Minh@DESKTOP-3QL3IC9>
2025-10-06 17:01:18 +0200

    Commit firmware, RTOS, ADMIT14

---
+++ F303_ADMIT14_RTOS/Debug/Core/Src/main.d
@@ -0,0 +1,111 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ ../Core/Inc/stm32f3xx_hal_conf.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303x8.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/can.h ../Core/Inc/dma.h \
+ ../Core/Inc/i2c.h ../Core/Inc/tim.h ../Core/Inc/gpio.h \
+ ../Core/Inc/lsm9ds1.h ../Core/Inc/LSM9DS1_Types.h ../Core/Inc/i2c.h \
+ ../Core/Inc/LSM9DS1_Registers.h
+../Core/Inc/main.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+../Core/Inc/stm32f3xx_hal_conf.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303x8.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+C:/Users/Minh/STM32Cube/Repository/STM32Cube_FW_F3_V1.11.5/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+../Core/Inc/adc.h:
+../Core/Inc/main.h:
+../Core/Inc/can.h:
+../Core/Inc/dma.h:
+../Core/Inc/i2c.h:
+../Core/Inc/tim.h:
+../Core/Inc/gpio.h:
+../Core/Inc/lsm9ds1.h:
+../Core/Inc/LSM9DS1_Types.h:
+../Core/Inc/i2c.h:
+../Core/Inc/LSM9DS1_Registers.h:
